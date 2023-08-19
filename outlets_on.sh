0aa2ee821375f0f4ac1a5aafa0e5692f9aed2001
Matthew Ramadan <matthewramadan@gmail.com>
2023-08-19 15:56:58 -0700

    Create outlets_on.sh

---
+++ outlets_on.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.3 i 1
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.4 i 1
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.5 i 1
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.6 i 1
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.7 i 1
+snmpset -v1 -c private x.x.x.x iso.3.6.1.4.1.3808.1.1.3.3.3.1.1.4.8 i 1
